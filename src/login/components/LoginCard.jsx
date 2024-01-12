cf96929a8b4c2b08f138632554db02beaad4736b
KepaMax <maljerov.1999@gmail.com>
2024-01-12 18:14:47 +0400

    Update

--- src/login/components/LoginCard.jsx
+++ src/login/components/LoginCard.jsx
@@ -8,7 +8,7 @@ function LoginCard() {
         <label>Email</label>
         <input type="email" className='border border-zinc-300 rounded-[6px] my-2' />
       </div>
-      <button type='submit' className='bg-[#f6b819] px-5 py-2 rounded-[7px]'>Submit</button>
+          <button type='submit' className='bg-[#f6b819] px-5 py-2 rounded-[7px] hover:bg-yellow-700'>Submit</button>
     </form>
   )
 }
--- src/mainpage/components/TodoCard.jsx
+++ src/mainpage/components/TodoCard.jsx
@@ -5,7 +5,7 @@ function TodoCard() {
         <div className='w-[560px] h-[330px] rounded-[20px] m-4'>
             <div className='flex py-5 flex-col rounded-t-[7px] max-h-[240px] items-center justify-center px-3 bg-zinc-200'>
                 <div className='w-full'>  
-                    <h3 className='text-3xl font-bold '>Card title</h3>
+                    <h3 className='text-3xl font-bold'>Card title</h3>
                 </div>
                 <p className='my-1'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
@@ -16,8 +16,8 @@ function TodoCard() {
                 </p>
             </div>
             <div className='flex justify-end items-center rounded-b-[7px]  bg-zinc-400 h-[90px]'>
-                <button className='mx-3 text-xl font-bold rounded-[7px] h-3/5 px-6 bg-[#f6b819]'>Edit</button>
-                <button className='mx-3 text-xl font-bold rounded-[7px] h-3/5 px-5 bg-[#f6b819]'>Delete</button>
+                <button className='mx-3 text-xl font-bold rounded-[7px] h-3/5 px-6 bg-[#f6b819] hover:bg-yellow-700'>Edit</button>
+                <button className='mx-3 text-xl font-bold rounded-[7px] h-3/5 px-5 bg-[#f6b819] hover:bg-yellow-700'>Delete</button>
             </div>
         </div>
     )
