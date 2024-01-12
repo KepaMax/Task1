c896efed040f2598da80d7810ffe77ff234d23a9
KepaMax <maljerov.1999@gmail.com>
2024-01-12 18:15:37 +0400

    Update 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TodoCard from './mainpage/components/TodoCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div className='text-3xl font-bold'>Hello World</div>
+      <TodoCard></TodoCard>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import TodoCard from './mainpage/components/TodoCard.jsx'
 import LoginCard from './login/components/LoginCard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoCard></TodoCard>
+    <React.StrictMode>
+        <App></App>
   </React.StrictMode>,
 )
