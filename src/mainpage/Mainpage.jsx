3711c4db6440edf30a55fa83883cef82234283b7
Kenan Muradov <maljerov.1999@gmail.com>
2024-01-16 17:40:08 +0300

    responsivenes update

--- src/mainpage/Mainpage.jsx
+++ src/mainpage/Mainpage.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 import TodoCard from './components/TodoCard'
 import Form from './components/Form'
@@ -12,13 +12,19 @@ function Mainpage({ setAuthorized, email }) {
     const [deleteVisibility, setDeleteVisibility] = useState(false)
 
     return (
-        <div className={`${editVisibility || createVisibility || deleteVisibility ? 'overflow-hidden' : ''} relative`}>
+        <div className={`relative ${editVisibility || createVisibility || deleteVisibility ? 'overflow-hidden' : ''}`}>
             <CreateCard createVisibility={createVisibility} setCreateVisibility={setCreateVisibility}></CreateCard>
             <DeleteCard deleteVisibility={deleteVisibility} setDeleteVisibility={setDeleteVisibility}></DeleteCard>
             <EditCard editVisibility={editVisibility} setEditVisibility={setEditVisibility}></EditCard>
             <Navbar email={email} setAuthorized={setAuthorized}></Navbar>
-            <button onClick={() => setCreateVisibility(true)} className='px-5 py-3 my-3 text-bold border rounded-[15px] mx-2 bg-amber-400 hover:bg-amber-500'>Create Card</button>
-            <div className='flex flex-wrap'>
+            <div className='px-2'>
+                <button onClick={() => setCreateVisibility(true)} className='w-full sm:w-max px-4 sm:px-5 py-2 sm:py-3 m my-2 sm:my-3 text-bold border rounded-[15px] bg-amber-400 hover:bg-amber-500'>Create Card</button>
+            </div>
+            <div className='flex flex-wrap items-stretch'>
+                <TodoCard setDeleteVisibility={setDeleteVisibility} setEditVisibility={setEditVisibility}></TodoCard>
+                <TodoCard setDeleteVisibility={setDeleteVisibility} setEditVisibility={setEditVisibility}></TodoCard>
+                <TodoCard setDeleteVisibility={setDeleteVisibility} setEditVisibility={setEditVisibility}></TodoCard>
+                <TodoCard setDeleteVisibility={setDeleteVisibility} setEditVisibility={setEditVisibility}></TodoCard>
                 <TodoCard setDeleteVisibility={setDeleteVisibility} setEditVisibility={setEditVisibility}></TodoCard>
                 <TodoCard setDeleteVisibility={setDeleteVisibility} setEditVisibility={setEditVisibility}></TodoCard>
                 <TodoCard setDeleteVisibility={setDeleteVisibility} setEditVisibility={setEditVisibility}></TodoCard>
