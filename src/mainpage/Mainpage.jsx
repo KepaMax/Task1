fc4c4ebfe3b1958c8bb4c9a676b9a1972669eced
KepaMax <maljerov.1999@gmail.com>
2024-01-19 21:36:46 +0400

    Reducer integrated

--- src/mainpage/Mainpage.jsx
+++ src/mainpage/Mainpage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from 'react'
+import { useEffect, useState, useContext, useReducer } from 'react'
 import Navbar from './components/Navbar'
 import TodoCard from './components/TodoCard'
 import CreateCard from './components/CreateCard'
@@ -7,11 +7,29 @@ import EditCard from './components/EditCard'
 import Context from '../ContextWrapper'
 
 function Mainpage() {
+
+    function reducer(state, action) {
+        switch (action.type) {
+            case 'create':
+                return { type: state.type = 'create' }
+            case 'edit':
+                return { type: state.type = 'edit' }
+            case 'delete':
+                return { type: state.type = 'delete' }
+            case 'reset':
+                return { type: state.type = '' }
+            default:
+                return state
+        }
+    }
+
     const { email } = useContext(Context)
     const [openModal, setOpenModal] = useState('')
     const [cards, setCards] = useState([])
     const [filteredCards, setFilteredCards] = useState([])
     const [activeCard, setActiveCard] = useState()
+    const [state, dispatch] = useReducer(reducer, { type: '' })
+
 
     useEffect(() => {
         setFilteredCards(cards.filter((card) => card.author === email));
@@ -20,31 +38,31 @@ function Mainpage() {
     return (
         <div className={`relative h-screen ${openModal ? 'overflow-hidden' : ''}`}>
 
-            {openModal === "create" ?
+            {state.type === "create" ?
                 <div className='fixed inset-0 flex justify-center items-center w-screen z-10 bg-opacity-50 bg-black'>
-                    <CreateCard setCards={setCards} setOpenModal={setOpenModal} email={email} />
+                    <CreateCard setCards={setCards} dispatch={dispatch} email={email} />
                 </div> : null
             }
 
-            {openModal === "edit" ?
+            {state.type === "edit" ?
                 <div className='fixed inset-0 flex justify-center items-center w-screen z-10 bg-opacity-50 bg-black'>
-                    <EditCard activeCard={activeCard} setCards={setCards} setOpenModal={setOpenModal} />
+                    <EditCard activeCard={activeCard} setCards={setCards} dispatch={dispatch} />
                 </div> : null
             }
 
-            {openModal === "delete" ?
+            {state.type === "delete" ?
                 <div className='fixed inset-0 flex justify-center items-center w-screen z-10 bg-opacity-50 bg-black'>
-                    <DeleteCard activeCard={activeCard} setCards={setCards} setOpenModal={setOpenModal} />
+                    <DeleteCard activeCard={activeCard} setCards={setCards} dispatch={dispatch} />
                 </div> : null
             }
 
             <Navbar />
             <div className='mx-2 lg:mx-5 px-2'>
-                <button onClick={() => setOpenModal('create')} className='w-full sm:w-max px-4 sm:px-5 py-2 sm:py-3 m my-2 sm:my-3 text-bold border rounded-[15px] bg-amber-400 hover:bg-amber-500'>Create Card</button>
+                <button onClick={() => dispatch({ type: 'create' })} className='w-full sm:w-max px-4 sm:px-5 py-2 sm:py-3 m my-2 sm:my-3 text-bold border rounded-[15px] bg-amber-400 hover:bg-amber-500'>Create Card</button>
             </div>
 
             <div className='grid sm:grid-cols-2 lg:grid-cols-3 p-2 lg:p-5'>
-                {filteredCards.length ? filteredCards.map((card) => (<TodoCard setActiveCard={setActiveCard} setOpenModal={setOpenModal} key={card.id} data={card} />)
+                {filteredCards.length ? filteredCards.map((card) => (<TodoCard setActiveCard={setActiveCard} dispatch={dispatch} key={card.id} data={card} />)
 
                 ) : <p className='col-span-3 text-center font-bold'>No cards found</p>}
             </div>
